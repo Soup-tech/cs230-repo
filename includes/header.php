ca25ba22a967ed99de5f5daff05ff6c1da7c3c75
soup-tech <campbellssoup21@gmail.com>
2021-03-11 12:13:16 -0500

    Lab 6

--- includes/header.php
+++ includes/header.php
@@ -35,22 +35,29 @@ session_start();
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <?php if (isset($_SESSION['uid'])) {
-                	    echo '<li class="nav-item">
-                    	<a class="nav-link" href="includes/logout.php">Logout</a>
+                	    echo 
+                        '<li class="nav-item">
+                    	    <a class="nav-link" href="includes/logout.php">Logout</a>
                         </li>
+
                         <li class="nav-item">
-                        <a class="nav-link" href="gallery.php">Gallery</a>
+                            <a class="nav-link" href="gallery.php">Gallery</a>
                         </li> 
+
                         <li class="nav-item">
-                        <a class="nav-link" href="profile.php">Profile</a>
-		                </li>';
+                            <a class="nav-link" href="profile.php">Profile</a>
+		                </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="admin.php">Admin</a>
+                        </li>';
 		            } else {
 			            echo '<li class="nav-item">
 			            <a class="nav-link" href ="login.php">Login</a>
 			            </li>';
 		            } ?>
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home</a>
+                        <a class="nav-link" href="../index.php">Home<span class="sr-only">(current)</span></a>
                     </li>
                 </ul>
             </div>
